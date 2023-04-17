5422685b717208c9fb7d908d310e6ec03ebcd98a
BoringDonut <alex@meanrin.cat>
2023-04-17 19:58:45 +0300

    Add custom collate_fn

--- audio_classifier/speech_commands_data_module.py
+++ audio_classifier/speech_commands_data_module.py
@@ -1,9 +1,20 @@
 from typing import Dict
 
 import numpy as np
+import torch
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 import lightning.pytorch as pl
+
+
+def collate_fn(batch):
+    # TODO: for some reason default collate can sometimes fail due to None values which is strange
+    audio = []
+    label = []
+    for b in batch:
+        audio.append(b["audio"])
+        label.append(b["label"])
+    return {"audio": torch.stack(audio, dim=0), "label": torch.LongTensor(label)}
 
 
 def get_sample(example) -> Dict[str, np.ndarray]:
@@ -40,18 +51,21 @@ class SpeechCommandsDataModule(pl.LightningDataModule):
                           batch_size=self.batch_size,
                           num_workers=self.num_workers,
                           shuffle=True,
-                          drop_last=True)
+                          drop_last=True,
+                          collate_fn=collate_fn)
 
     def val_dataloader(self):
         return DataLoader(self.dataset["validation"].map(get_sample, num_proc=max(1, self.num_workers)),
                           batch_size=self.batch_size,
                           num_workers=self.num_workers,
                           shuffle=False,
-                          drop_last=False)
+                          drop_last=False,
+                          collate_fn=collate_fn)
 
     def test_dataloader(self):
         return DataLoader(self.dataset["test"].map(get_sample, num_proc=max(1, self.num_workers)),
                           batch_size=self.batch_size,
                           num_workers=self.num_workers,
                           shuffle=False,
-                          drop_last=False)
+                          drop_last=False,
+                          collate_fn=collate_fn)
